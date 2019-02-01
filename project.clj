6b29032a7509c1d878ec2b7595441349d6122dc4
Moe Aboulkheir <moe@nervous.io>
2019-02-01 00:48:11 +0000

    Remove LICENSE cruft

--- project.clj
+++ project.clj
@@ -8,11 +8,15 @@
   :dependencies [[org.clojure/clojure       "1.10.0"]
                  [aleph                     "0.4.6"]
                  [io.datopia/stickler-codec "0.1.0"]]
-  :profiles     {:dev {:dependencies [[io.datopia/stickler-translate "0.1.0"]
-                                      [org.clojure/test.check        "0.10.0-alpha3"]]
-                       :plugins      [[lein-codox "0.10.5"]]
-                       :codox        {:namespaces [#"^abci\.(?!impl)"]
-                                      :metadata   {:doc/format :markdown}}
-                       :global-vars  {*warn-on-reflection* true}
-                       :aliases
-                       {"gen-protobuf" ["run" "-m" "abci.impl.gen-protobuf"]}}})
+  :profiles
+  {:dev
+   {:dependencies [[io.datopia/stickler-translate "0.1.0"]
+                   [io.datopia/codox-theme        "0.1.0"]
+                   [org.clojure/test.check        "0.10.0-alpha3"]]
+    :plugins      [[lein-codox "0.10.5"]]
+    :codox        {:namespaces [#"^abci\.(?!impl)"]
+                   :metadata   {:doc/format :markdown}
+                   :themes     [:default [:datopia {:datopia/github "https://github.com/datopia/abci-host/"}]]}
+    :global-vars  {*warn-on-reflection* true}
+    :aliases
+    {"gen-protobuf" ["run" "-m" "abci.impl.gen-protobuf"]}}})
